b8e0422d1c2f3a97c6fa7d95686786f9078dd16f
Joel Jensen <yobert@gmail.com>
2020-12-06 22:55:43 -0700

    init: Fix go mod init command to use basename of PWD

--- duck/cmd/duck/init.go
+++ duck/cmd/duck/init.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"path"
 	"os"
 	"os/exec"
 )
 
 func initialize() error {
 	if _, err := os.Stat("go.mod"); err != nil {
-		cmd := exec.Command("go", "mod", "init")
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		base := path.Base(wd)
+
+		cmd := exec.Command("go", "mod", "init", base)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
